fb12900f6aa7b4b5c1331fb4873a1cdd81599b50
‘realjiang <superjishere@163.com>
2020-12-08 12:56:58 +0800

    feat:新增[多态]编程练习1

---
+++ basic/src/objectoriented/_05polymorphism/_02test1/info/Dog.java
@@ -0,0 +1,16 @@
+package objectoriented._05polymorphism._02test1.info;
+
+/**
+ * 标题:
+ *
+ * @Author: Jiang
+ * @Date: 2020/12/8 11:14
+ */
+public class Dog extends Animal{
+    public Dog() {
+    }
+    @Override
+    public void cry() {
+        System.out.println("小狗的叫声:汪汪汪~~~");
+    }
+}
