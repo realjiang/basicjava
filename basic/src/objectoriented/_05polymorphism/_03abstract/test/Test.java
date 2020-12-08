176afdd6c90c32c92912d72de614245865274e57
‘realjiang <superjishere@163.com>
2020-12-08 13:51:03 +0800

    feat:新增[abstract]编程练习-1

---
+++ basic/src/objectoriented/_05polymorphism/_03abstract/test/Test.java
@@ -0,0 +1,20 @@
+package objectoriented._05polymorphism._03abstract.test;
+
+import objectoriented._05polymorphism._03abstract.info.Circle;
+import objectoriented._05polymorphism._03abstract.info.Rectangle;
+import objectoriented._05polymorphism._03abstract.info.Shape;
+
+/**
+ * 标题: 测试类-求面积
+ *
+ * @Author: Jiang
+ * @Date: 2020/12/8 13:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        Shape s1 = new Circle(4);
+        Shape s2 = new Rectangle(6,5);
+        System.out.println("圆的面积为:"+s1.area());
+        System.out.println("矩形的面积为:"+s2.area());
+    }
+}
