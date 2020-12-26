2b8167cc13e28d2ecf7001ebfda6d42d9ac8006e
realjiang <superjishere@163.com>
2020-12-26 17:39:22 +0800

    feat:新增[线程]创建方式一：通过继承Thread类实现  带名字的 线程

--- basic/src/_07thread/_1create/ThreadTest1.java
+++ basic/src/_07thread/_1create/ThreadTest1.java
@@ -3,18 +3,21 @@ package src._07thread._1create;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 /**
- * 为啥继承Thread类就是 线程了？
- * 规定，java底层已经写好了，按照这种方式做就能得到线程
+ * 继承Thread类来实现线程
  */
-class MyThread extends Thread {
+class MyThread1 extends Thread {
+    /**
+     * 为啥继承Thread类就是 线程了？
+     * 规定，java底层已经写好了，按照这种方式做就能得到线程
+     */
     public  void run() {
         System.out.println(getName()+"线程正在执行！");
     }
 }
-public class ThreadTest {
+public class ThreadTest1 {
     public static void main(String[] args) {
         System.out.println("主线程1");
-        MyThread mt = new MyThread();
+        MyThread1 mt = new MyThread1();
         mt.start();
         //mt.start();//启动线程(线程只能启动一次，超过一次会抛出异常Exception in thread "main" java.lang.IllegalThreadStateException)
         System.out.println("主线程2");
---
+++ basic/src/_07thread/_1create/ThreadTest2.java
@@ -0,0 +1,24 @@
+package src._07thread._1create;
+
+/**
+ * 继承Thread类来实现线程 + 参数name
+ */
+class MyThread2 extends Thread {
+    public MyThread2(String name) {
+        super(name);//调用父类构造方法
+    }
+    public  void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(getName()+"正在运行"+i);
+        }
+    }
+}
+
+public class ThreadTest2 {
+    public static void main(String[] args) {
+        MyThread2 mt1 = new MyThread2("线程1");
+        MyThread2 mt2 = new MyThread2("线程2");
+        mt1.start();
+        mt2.start();
+    }
+}
