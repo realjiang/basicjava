cc87412320e779e176297024f6765e067ba9937b
realjiang <superjishere@163.com>
2021-01-18 13:22:08 +0800

    feat:新增[Lambda表达式]@FunctionalInterface注解

--- src/advanced/_05lambda/demo1/MathOperation.java
+++ src/advanced/_05lambda/demo1/MathOperation.java
@@ -3,6 +3,7 @@ package advanced._05lambda.demo1;
 /**
  * 四则运算接口
  */
+@FunctionalInterface //该注解可省略,加上后,会在编译时进行检查,否则运行时才报错.作用是通知编译器这是函数式接口,进行抽象方法检查
 public interface MathOperation {
     public Float operate(Integer a ,Integer b);
 }
